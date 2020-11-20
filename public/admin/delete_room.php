8597366751b1ca357f657669922c0c3ea6cf0168
ansel2000 <ansel20@gmail.com>
2020-11-20 20:42:57 +0530

    delete room completed
    delete room completed

--- public/admin/delete_room.php
+++ public/admin/delete_room.php
@@ -1,5 +1,6 @@
 <?php
   require("../config.php");
+  ob_start();
 ?>
 
 <?php include("./includes/header.php"); ?>
@@ -22,8 +23,110 @@
             
       $row = getRoomFromId($pdo, $id);
 
-      print("<pre>".print_r($row, true)."</pre>");
-    ?>
+?>
+    <h2 class="text-center">Are you sure?</h2>
+    <table class="table table-bordered text-center">
+  <thead>
+    <tr>
+      <th scope="col">Fields</th>
+      <th scope="col">Details</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+     
+      <td>Room I.D.</td>
+      <td><?=$row['room_id']?></td>
+    </tr>
+    <tr>
+      
+      <td>Room Number</td>
+      <td><?=$row['room_number']?></td>
+    </tr>
+    <tr>
+      <td >Room Name</td>
+      <td><?=$row['room_name']?></td>
+    </tr>
+    <tr>
+      <td >Room Type</td>
+      <td><?=$row['room_type']?></td>
+    </tr>
+    <tr>
+      <td >Room Featured</td>
+      <td><?=$row['room_featured']==1?'Yes':'No';?></td>
+    </tr>
+    <tr>
+      <td >Room Booked</td>
+      <td><?=$row['room_price']==1? 'Yes': 'No';?></td>
+    </tr>
+<?php
+if($row['room_booked']==1){
+?>
+      <tr>
+      <td >Check In Data</td>
+      <td><?=$row['check_in_date']?></td>
+    </tr>
+    <tr>
+      <td >Check Out Date</td>
+      <td><?=$row['check_out_date']?></td>
+    </tr>
+<?php
+    }
+?>
+    <tr>
+      <td >Room Floor</td>
+      <td><?=$row['room_floor']?></td>
+    </tr>
+
+    <tr>
+      <td >Room View</td>
+      <td><?=$row['room_view']?></td>
+    </tr><tr>
+      <td >Room Beds</td>
+      <td><?=$row['room_beds']?></td>
+    </tr>
+    <tr>
+      <td >Bed Type</td>
+      <td><?=$row['bed_type']?></td>
+    </tr>
+    <tr>
+      <td >Room Capacity</td>
+      <td><?=$row['room_capacity']?></td>
+    </tr>
+    <tr>
+      <td >Room Amenities</td>
+      <td><?=$row['room_amenities']?></td>
+    </tr>
+
+  </tbody>
+</table>
+<br>
+ <form method="POST">
+ <div class="row" style="margin-left: 15%;">
+ <div class="col">
+    <button class="btn btn-danger" name="submit">Submit</button>
+</div>
+<div class="col">
+    <button class="btn btn-primary" name="cancel" style="margin-left: 15%;">Cancel</button>
+</div>
+</div>
+ </form>   
+
+<?php
+      // print("<pre>".print_r($row, true)."</pre>");
+      if(isset($_POST["submit"])){
+        $room_number=$row['room_number'];
+        $sql = "DELETE FROM rooms WHERE room_number = $room_number LIMIT 1;";
+        $statement = $pdo->prepare($sql);
+        $statement->execute();
+        header("Location: rooms.php");
+        die();
+      }
+      if(isset($_POST["cancel"])){
+        header("Location: rooms.php");
+      }
+      ob_end_flush();
+?>
 
 </section>
 </body>
