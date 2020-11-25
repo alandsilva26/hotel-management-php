7611a6eca34e3fde38ebdd2192ddbb27e750812e
Alan Dsilva <alandsilva2001@gmail.com>
2020-11-25 10:18:30 +0530

    Add updated reservation form

--- public/reservation.php
+++ public/reservation.php
@@ -1,258 +1,122 @@
 <?php
-require_once("./config.php");
-include("./includes/header.php");
+include("./config.php");
+
+// Check if user is already logged in
 ?>
-<body>
-  <header>
-      <?php include("./includes/navbar.php"); ?>
-  </header>
 
+<?php include("./includes/header.php"); ?>
   <body>
-    <style>
-      .section {
-        position: relative;
-        height: 100vh;
-      }
+    <?php include("./includes/navbar.php"); ?>
 
-      .section .section-center {
-        position: absolute;
-        top: 50%;
-        left: 0;
-        right: 0;
-        -webkit-transform: translateY(-50%);
-        transform: translateY(-50%);
-      }
-
-      #booking::after {
-        /* font-family: "Montserrat", sans-serif; */
-        background-image: url("home-header-1.jpg") no-repeat;
-        background-size: cover;
-        background-position: center;
-      }
-
-      #booking::before {
-        content: "";
-        position: absolute;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        top: 0;
-        background: rgba(47, 103, 177, 0.6);
-      }
-
-      .booking-form {
-        background-color: #fff;
-        padding: 50px 20px;
-        -webkit-box-shadow: 0px 5px 20px -5px rgba(0, 0, 0, 0.3);
-        box-shadow: 0px 5px 20px -5px rgba(0, 0, 0, 0.3);
-        border-radius: 4px;
-      }
-
-      .booking-form .form-group {
-        position: relative;
-        margin-bottom: 30px;
-      }
-
-      .booking-form .form-control {
-        background-color: #ebecee;
-        border-radius: 4px;
-        border: none;
-        height: 40px;
-        -webkit-box-shadow: none;
-        box-shadow: none;
-        color: #3e485c;
-        font-size: 14px;
-      }
-
-      .booking-form .form-control::-webkit-input-placeholder {
-        color: rgba(62, 72, 92, 0.3);
-      }
-
-      .booking-form .form-control:-ms-input-placeholder {
-        color: rgba(62, 72, 92, 0.3);
-      }
-
-      .booking-form .form-control::placeholder {
-        color: rgba(62, 72, 92, 0.3);
-      }
-
-      .booking-form input[type="date"].form-control:invalid {
-        color: rgba(62, 72, 92, 0.3);
-      }
-
-      .booking-form select.form-control {
-        -webkit-appearance: none;
-        -moz-appearance: none;
-        appearance: none;
-      }
-
-      .booking-form select.form-control + .select-arrow {
-        position: absolute;
-        right: 0px;
-        bottom: 4px;
-        width: 32px;
-        line-height: 32px;
-        height: 32px;
-        text-align: center;
-        pointer-events: none;
-        color: rgba(62, 72, 92, 0.3);
-        font-size: 14px;
-      }
-
-      .booking-form select.form-control + .select-arrow:after {
-        content: "\279C";
-        display: block;
-        -webkit-transform: rotate(90deg);
-        transform: rotate(90deg);
-      }
-
-      .booking-form .form-label {
-        display: inline-block;
-        color: #3e485c;
-        font-weight: 700;
-        margin-bottom: 6px;
-        margin-left: 7px;
-      }
-
-      .booking-form .submit-btn {
-        display: inline-block;
-        color: #fff;
-        background-color: #1e62d8;
-        font-weight: 700;
-        padding: 14px 30px;
-        border-radius: 4px;
-        border: none;
-        -webkit-transition: 0.2s all;
-        transition: 0.2s all;
-      }
-
-      .booking-form .submit-btn:hover,
-      .booking-form .submit-btn:focus {
-        opacity: 0.9;
-      }
-
-      .booking-cta {
-        margin-top: 80px;
-        margin-bottom: 30px;
-      }
-
-      .booking-cta h1 {
-        font-size: 52px;
-        text-transform: uppercase;
-        color: #fff;
-        font-weight: 700;
-      }
-
-      .booking-cta p {
-        font-size: 16px;
-        color: rgba(255, 255, 255, 0.8);
-      }
-    </style>
-    <div id="booking" class="section">
-      <div class="section-center">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-7 col-md-push-5">
-              <div class="booking-cta">
-                <h1>Make your reservation</h1>
+<section id="reservation">
+   <div class="container mt-5">
+      <div class="row">
+         <div class="col-md-5">
+         <h1>Make your reservation</h1>
                 <p>
                   Our hotel is self-certified to follow a series of
                   precautionary measures to make your hotel stay safe and
                   healthy.
                 </p>
-              </div>
-            </div>
-            <div class="col-md-4 col-md-pull-7">
-              <div class="booking-form">
-                <form method="POST" action="bill_view.php">
-                  <div class="form-group">
-                    <span class="form-label">Enter User I.D.</span>
-                    <input
-                      class="form-control"
-                      type="text"
-                      name="user_id"
-                      placeholder="Enter your user I.D."
-                      required
-                    />
-                  </div>
-                  <div class="form-group">
-                    <span class="form-label">Phone Number</span>
-                    <input
-                      class="form-control"
-                      type="number"
-                      name="phone_number"
-                      placeholder="Enter your phone number"
-                      required
-                    />
-                  </div>
-                  <div class="form-group">
-                    <span class="form-label">Room Type</span>
-                    <select class="form-control" name="Room_Type" required>
-                      <option value="">Select</option>
-                      <option value="AC">classic Double room</option>
-                      <option value="AC">classic Double room</option>
-                      <option value="AC">classic Double room</option>
-                      <option value="AC">classic Double room</option>
-                    </select>
-                  </div>
-                  <div class="row">
-                    <div class="col-sm-6">
-                      <div class="form-group">
+         </div>
+         <div class="col-md-7">
+            <form method="POST" action="bill_view.php">
+               <div class="form-group">
+                  <span class="form-label">Enter User I.D.</span>
+                  <input
+                     class="form-control"
+                     type="text"
+                     name="user_id"
+                     placeholder="Enter your user I.D."
+                     required
+                     />
+               </div>
+               <div class="form-group">
+                  <span class="form-label">Phone Number</span>
+                  <input
+                     class="form-control"
+                     type="number"
+                     name="phone_number"
+                     placeholder="Enter your phone number"
+                     required
+                     />
+               </div>
+               <div class="form-group">
+                  <span class="form-label">Room Type</span>
+                  <select class="form-control" name="Room_Type" required>
+                     <option value="">Select</option>
+                     <option value="AC">classic Double room</option>
+                     <option value="AC">classic Double room</option>
+                     <option value="AC">classic Double room</option>
+                     <option value="AC">classic Double room</option>
+                  </select>
+               </div>
+               <div class="row">
+                  <div class="col-sm-6">
+                     <div class="form-group">
                         <span class="form-label">Check In</span>
                         <input class="form-control" name="check_in_date" type="date" required />
-                      </div>
-                    </div>
-                    <div class="col-sm-6">
-                      <div class="form-group">
+                     </div>
+                  </div>
+                  <div class="col-sm-6">
+                     <div class="form-group">
                         <span class="form-label">Check out</span>
                         <input class="form-control" type="date" required />
-                      </div>
-                    </div>
+                     </div>
                   </div>
-                  <div class="row">
-                    <!-- <div class="col-sm-4">
-                      <div class="form-group">
-                        <span class="form-label">Rooms</span>
-                        <select class="form-control" required>
-                          <option>1</option>
-                          <option>2</option>
-                          <option>3</option>
-                        </select>
-                        <span class="select-arrow"></span>
-                      </div>
-                    </div> -->
-                    <div class="col-sm-6">
-                      <div class="form-group">
+               </div>
+               <div class="row">
+                  <!-- <div class="col-sm-4">
+                     <div class="form-group">
+                       <span class="form-label">Rooms</span>
+                       <select class="form-control" required>
+                         <option>1</option>
+                         <option>2</option>
+                         <option>3</option>
+                       </select>
+                       <span class="select-arrow"></span>
+                     </div>
+                     </div> -->
+                  <div class="col-sm-6">
+                     <div class="form-group">
                         <span class="form-label">Adults</span>
                         <select class="form-control" name="no_adults" required>
-                          <option>1</option>
-                          <option>2</option>
-                          <option>3</option>
+                           <option>1</option>
+                           <option>2</option>
+                           <option>3</option>
                         </select>
                         <span class="select-arrow"></span>
-                      </div>
-                    </div>
-                    <div class="col-sm-6">
-                      <div class="form-group">
+                     </div>
+                  </div>
+                  <div class="col-sm-6">
+                     <div class="form-group">
                         <span class="form-label">Children</span>
                         <select class="form-control" name="no_children" required>
-                          <option>0</option>
-                          <option>1</option>
-                          <option>2</option>
+                           <option>0</option>
+                           <option>1</option>
+                           <option>2</option>
                         </select>
                         <span class="select-arrow"></span>
-                      </div>
-                    </div>
+                     </div>
                   </div>
-                  <div class="form-btn">
-                    <button class="submit-btn" name="confirm_booking">Confirm Booking</button>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
+               </div>
+               <div class="form-btn">
+                  <button class="submit-btn" name="confirm_booking">Confirm Booking</button>
+               </div>
+            </form>
+         </div>
       </div>
-    </div>
+   </div>
+</section>
+    
+    <?php include("./includes/footer.php"); ?>
+    <script>
+    $(document).ready(function() {
+        $("nav").eq(0).addClass("bg-dark");
+        $("nav").eq(0).addClass("navbar-dark");
+        // bg-dark navbar-dark 
+    });
+  </script>
+    
   </body>
 </html>
