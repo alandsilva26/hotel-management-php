50624dac53c8fa1343cba58fc5d0332e0c459923
Alan Dsilva <alandsilva2001@gmail.com>
2020-11-29 13:41:18 +0530

    Merge

--- public/core/reservation_controller.php
+++ public/core/reservation_controller.php
@@ -1,6 +1,16 @@
 <?php
+require("../config.php");
+header("Content-Type: application/json; charset=utf-8");
+
 
     if (isset($_POST["action"])) {
         if ($_POST["action"] == "reservation") {
+            $_SESSION["reservation"] = array(
+                "check_in_date" => "",
+                "check_out_date" => "",
+                "no_children" => "",
+                "no_adults" => "",
+                "room_id" => "",
+            );
         }
     }
--- public/payment.php
+++ public/payment.php
@@ -1,18 +1,15 @@
 <?php
   require_once("./config.php");
-?>
-<?php include("./includes/header.php"); ?>
-
-<?php
+   include("./includes/header.php");
 require  '../vendor/autoload.php';
 
 if (isset($_POST['confirm_booking'])) {
     $room_type=$_POST['room_type'];
     $user_id=$_POST["user_id"];
     $ph_number=$_POST["phone_number"];
-    $check_in_date = date('d-m-Y',strtotime($_POST["check_in_date"]));
+    $check_in_date = date('d-m-Y', strtotime($_POST["check_in_date"]));
     
-    $check_out_date = date('d-m-Y',strtotime($_POST["check_out_date"]));
+    $check_out_date = date('d-m-Y', strtotime($_POST["check_out_date"]));
     
     $no_adults=$_POST["no_adults"];
     $no_children=$_POST["no_children"];
@@ -39,10 +36,10 @@ if (isset($_POST['confirm_booking'])) {
     $statement = $pdo->prepare($sql2);
     $statement->execute();
 
-    $sql3="SELECT reservation_id FROM reservations WHERE user_id=\"$user_id\" LIMIT 1;";   
+    $sql3="SELECT reservation_id FROM reservations WHERE user_id=\"$user_id\" LIMIT 1;";
     $statement = $pdo->prepare($sql3);
     $statement->execute();
-    $row2 = $statement->fetch(PDO::FETCH_ASSOC); 
+    $row2 = $statement->fetch(PDO::FETCH_ASSOC);
     $reservation=$row2['reservation_id'];
 
     $sql4="INSERT INTO `payment`(`user_id`, `reservation_id`, `currency`, `method`, `amount`) VALUES ($user_id, $reservation,\"INR\",\"card\",$total_cost);
@@ -50,10 +47,10 @@ if (isset($_POST['confirm_booking'])) {
     $statement = $pdo->prepare($sql4);
     $statement->execute();
 
-    $sql5="SELECT payment_id FROM payment WHERE reservation_id=\"$reservation\" LIMIT 1;";   
+    $sql5="SELECT payment_id FROM payment WHERE reservation_id=\"$reservation\" LIMIT 1;";
     $statement = $pdo->prepare($sql5);
     $statement->execute();
-    $row3 = $statement->fetch(PDO::FETCH_ASSOC); 
+    $row3 = $statement->fetch(PDO::FETCH_ASSOC);
 
     $_SESSION['payment_id']=$row3['payment_id'];
     $_SESSION['amount_paid']= $total_cost;
@@ -64,90 +61,144 @@ if (isset($_POST['confirm_booking'])) {
     $_SESSION['no_days']=$date_diff;
 }
 ?>
-
-<body>
-
-
-<div class="container mt-5" >
-    <div class="row">
-        <div class="col-xs-12 col-md-4 offset-md-4">
-            <div class="card">
-                <div class="card-header">
-                    <div class="row">
-                        <h3 class="text-xs-center">Payment Details</h3>
-                        <img class="img-fluid cc-img" src="http://www.prepbootstrap.com/Content/images/shared/misc/creditcardicons.png">
-                    </div>
-                </div>
-                <div class="card-body">
-                    <form role="form">
-                    <div class="flex-row">
-                            <div class="col-xs-12 ">
-                                <div class="form-group">
-                                    <label>Amount</label>
-                                    <div>
-                                        <h2><b>&#8377; <?= $total_cost ?></b><h2>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="flex-row">
-                            <div class="col-xs-12 ">
-                                <div class="form-group">
-                                    <label>CARD NUMBER</label>
-                                    <div class="input-group">
-                                        <input type="tel" class="form-control" placeholder="Valid Card Number" />
-                                        <span class="input-group-addon"><span class="fa fa-credit-card"></span></span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+  <body>
+    <?php include("./includes/navbar.php"); ?>
+<div class="container mt-5">
+            <div class="row justify-content-between">
+               <div class="col-md-5">
+               <h1>Make your reservation</h1>
+                     <p>
+                        Our hotel is self-certified to follow a series of
+                        precautionary measures to make your hotel stay safe and
+                        healthy.
+                     </p>
+               </div>
+               <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header">
                         <div class="row">
-                            <div class="col-xs-7 col-md-7">
-                                <div class="form-group">
-                                    <label><span class="hidden-xs">EXPIRATION</span><span class="visible-xs-inline">EXP</span> DATE</label>
-                                    <input type="tel" class="form-control" placeholder="MM / YY" />
-                                </div>
-                            </div>
-                            <div class="col-xs-5 col-md-5 float-xs-right">
-                                <div class="form-group">
-                                    <label>CV CODE</label>
-                                    <input type="tel" class="form-control" placeholder="CVC" />
-                                </div>
-                            </div>
+                            <h3 class="text-xs-center">Payment Details</h3>
+                            <img class="img-fluid cc-img" src="http://www.prepbootstrap.com/Content/images/shared/misc/creditcardicons.png">
                         </div>
+                    </div>
+                    <div class="card-body">
+                        <form role="form">
+                        <div class="flex-row">
+                                <div class="col-xs-12 ">
+                                    <div class="form-group">
+                                        <label>Amount</label>
+                                        <div>
+                                            <h2><b>$ 500</b><h2>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="flex-row">
+                                <div class="col-xs-12 ">
+                                    <div class="form-group">
+                                        <label>CARD NUMBER</label>
+                                        <div class="input-group">
+                                            <input type="tel" class="form-control" placeholder="Valid Card Number" />
+                                            
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-7 col-md-7">
+                                    <div class="form-group">
+                                        <label><span class="hidden-xs">EXPIRATION</span><span class="visible-xs-inline">EXP</span> DATE</label>
+                                        <input type="tel" class="form-control" placeholder="MM / YY" />
+                                    </div>
+                                </div>
+                                <div class="col-xs-5 col-md-5 float-xs-right">
+                                    <div class="form-group">
+                                        <label>CV CODE</label>
+                                        <input type="tel" class="form-control" placeholder="CVC" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="flex-row">
+                                <div class="col-xs-12">
+                                    <div class="form-group">
+                                        <label>CARD OWNER</label>
+                                        <input type="text" class="form-control" placeholder="Card Owner Names" />
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="card-footer">
                         <div class="flex-row">
                             <div class="col-xs-12">
-                                <div class="form-group">
-                                    <label>CARD OWNER</label>
-                                    <input type="text" class="form-control" placeholder="Card Owner Names" />
-                                </div>
+                                <button class="btn btn-warning btn-lg btn-block">Process payment</button>
                             </div>
-                        </div>
-                    </form>
-                </div>
-                <div class="card-footer">
-                    <div class="flex-row">
-                        <div class="col-xs-12">
-                        <form method='POST' action="bill_view.php">
-                            <button class="btn btn-warning btn-lg btn-block">Process payment</button>
-                        </form>
                         </div>
                     </div>
                 </div>
+               </div>
+
+               <!-- <div class="col-md-1"></div> -->
             </div>
-        </div>
-    </div>
-</div>
+         </div>
+    
+<?php include("./includes/footer.php"); ?>
+    <script>
+   $("#check_in_date").datepicker({
+    minDate: 0,
+    numberOfMonths: [1, 2],
+    onSelect: function (data) {
+       console.log(data);
+      // $("#check-out").datepicker("show");
+      $("#check_in_date").html($("#check_in_date").val());
+    },
+  });
 
-<style>
-    .cc-img {
-        margin: 0 auto;
-    }
-</style>
+  $("#check_out_date").datepicker({
+    minDate: 1,
+    numberOfMonths: [1, 2],
+    onSelect: function () {
+      // $("#check-out").datepicker("show");
+      $("#check_out_date").html($("#check_out_date-in").val());
+    },
+  });
 
-</body>
+    $(document).ready(function() {
+      //  console.log(new Date($("#check_in_date").val()));
+        $("nav").eq(0).addClass("bg-dark");
+        $("nav").eq(0).addClass("navbar-dark");
+        // bg-dark navbar-dark 
+    });
 
+    $("#reservation_details").submit(function (e) {
+      // $(".error-message").html("");
+      // $(".form-control").removeClass("is-invalid");
+      // $("#login-user-btn").html("Loggin you in...");
 
-
+      e.preventDefault();
+      var formData = new FormData(this);
+      $.ajax({
+        url: "core/reservation_controller.php",
+        type: "POST",
+        data: formData,
+        success: function (data) {
+          if (data.error == 1) {
+          
+          } else {
+            // window.location.href="verify_user.php";
+            // return;
+          }
+        },
+        error: function (data, message, errorThrown) {
+          // $("#error-form").html("<span class=\"p-2\">" + message + errorThrown + "</span>");
+        },
+        cache: false,
+        contentType: false,
+        processData: false
+      });
+    });
+  </script>
+    
+  </body>
 </html>
- 
+>>>>>>> Stashed changes
